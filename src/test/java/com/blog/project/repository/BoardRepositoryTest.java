73af7b1f70223d8f6f4bcba96fad8b961331a9a4
KYUNGMIN <nkm4095@naver.com>
2022-12-11 14:18:07 +0900

    BoardRepository  UnitTest

---
+++ src/test/java/com/blog/project/repository/BoardRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.blog.project.repository;
+
+
+
+
+import com.blog.project.model.Board;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+public class BoardRepositoryTest {
+
+    private final Logger log = LoggerFactory.getLogger(BoardRepositoryTest.class);
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    @DisplayName("게시글 작성 테스트")
+    public void insertBoard(){
+        Board board=new Board(1,"title","content");
+        Board boardEntity=boardRepository.save(board);
+
+        assertNotNull(boardEntity);
+        assertEquals(board.getTitle(),boardEntity.getTitle());
+        log.info("게시글 작성 테스트 성공");
+
+    }
+
+    @Test
+    @DisplayName("게시글 수정 테스트")
+    public void modifyBaord(){
+        Board board=new Board(1,"title","content");
+        boardRepository.save(board);
+
+        board.setContent("This is Content");
+        Board boardEntity2=boardRepository.save(board);
+
+        assertEquals(board.getContent(),boardEntity2.getContent());
+        log.info("게시글 수정 테스트 성공");
+
+    }
+
+    @Test
+    @DisplayName("게시글 삭제 테스트")
+    public void deleteBoard(){
+        Board board=new Board(1,"title","content");
+        boardRepository.save(board);
+        boardRepository.delete(board);
+
+
+        assertEquals(Optional.empty(),boardRepository.findById(1));
+
+        log.info("게시글 삭제 테스트 성공");
+    }
+}
